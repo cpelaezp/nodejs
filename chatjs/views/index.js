8cd1018a62a4e53fd48e64e8726bba0f302ba782
cpeaezp <cpelaezp@gmail.com>
2017-03-20 23:37:35 -0500

    first commit

--- chatjs/views/index.js
+++
@@ -1,8 +0,0 @@
-var app = require('express');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var path = require('path');
-
-app.get('/', function(req, resp){
-     
-});
